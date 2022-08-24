f4342ffbd1a926d06478b8c71fa54a28b0778763
Danica Yener <ny21037@googlemail.com>
2022-08-24 17:34:09 +0200

    .

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
+// class Employee {
+// 	constructor(firstname, lastname) {
+// 	this.firstname = firstname;
+// 	this.lastname = lastname;
+// 	this.fullname = `${firstname} ${lastname}`;
+// 	this.email = `${firstname.toLowerCase()}.${lastname.toLowerCase()}@company.com`;
+	
+// 	// Complete the code!
+// 	}
+// 	}
+	
+// 	const student1 = new Employee("Alan", "Diko");
+	
+// 	console.log(student1);
+	
+// 	const emp1 = new Employee("John", "Smith");
+	
+	
+// 	console.log(emp1.fullname);
+// 	console.log(emp1.firstname);
+// 	console.log(emp1.email);
+// 	console.log(student1.email); 
+
+
 class Employee {
-	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
+	constructor(firstname, lastname) {
+	this.firstname = firstname;
+	this.lastname = lastname;
 	}
-}
+	
+	createFullName() {
+	console.log(`${this.firstname} ${this.lastname}`);
+	}
+	
+	createEmail() {
+	console.log(
+	`${this.firstname.toLowerCase()}.${this.lastname.toLowerCase()}@company.com`
+	);
+	}
+	
+	// Complete the code!
+	}
+	
+	const student1 = new Employee("Daniel", "Reiller");
+	console.log(student1);
+	
+	student1.createFullName();
+	student1.createEmail(); 
